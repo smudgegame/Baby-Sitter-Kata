a44884c421400a0027773f9a52294b4cc57d0407
smudgegame <atmargraff@gmail.com>
2019-11-16 21:00:50 -0500

    don't always throw exception

--- src/main/java/BabySitter.java
+++ src/main/java/BabySitter.java
@@ -1,5 +1,10 @@
+
 public class BabySitter {
     public int calculatePay(String start, String end) {
-        throw new IllegalArgumentException();
+        TimeParser timeParser = new TimeParser();
+        int startHour = timeParser.parse(start);
+        int endHour = timeParser.parse(end);
+        if(endHour < startHour) throw new IllegalArgumentException();
+        return 10;
     }
 }
--- src/test/java/BabySitterTest.java
+++ src/test/java/BabySitterTest.java
@@ -10,4 +10,10 @@ public class BabySitterTest {
         babySitter.calculatePay("6:00pm", "5:00pm");
     }
 
+    @Test
+    public void oneHourForTenDollars() {
+        BabySitter babySitter = new BabySitter();
+        int pay = babySitter.calculatePay("5:00pm", "6:00pm");
+        assertEquals(10, pay);
+    }
 }
