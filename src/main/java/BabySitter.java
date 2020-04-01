6a2849f1a656b235f7e56960e4ffda60073f3e47
smudgegame <atmargraff@gmail.com>
2020-03-31 21:17:36 -0400

    Restricts babysitter to allowable times

--- src/main/java/BabySitter.java
+++ src/main/java/BabySitter.java
@@ -4,7 +4,8 @@ public class BabySitter {
         TimeParser timeParser = new TimeParser();
         int startHour = timeParser.parse(start);
         int endHour = timeParser.parse(end);
-        if(endHour < startHour) throw new IllegalArgumentException();
+        if(endHour < startHour || startHour < 17 || endHour > 28)
+            throw new IllegalArgumentException();
         return 10;
     }
 }
--- src/test/java/BabySitterTest.java
+++ src/test/java/BabySitterTest.java
@@ -10,11 +10,25 @@ public class BabySitterTest {
         babySitter.calculatePay("6:00pm", "5:00pm");
     }
 
+    @Test(expected =IllegalArgumentException.class)
+    public void timeBeforeAllowableHours(){
+        BabySitter babySitter = new BabySitter();
+        babySitter.calculatePay("4:00pm","6:00pm");
+    }
+
+    @Test(expected =IllegalArgumentException.class)
+    public void timeAfterAllowableHours(){
+        BabySitter babySitter = new BabySitter();
+        babySitter.calculatePay("5:00pm","6:00am");
+    }
+
     @Test
     public void oneHourForTenDollars() {
         BabySitter babySitter = new BabySitter();
         int pay = babySitter.calculatePay("5:00pm", "6:00pm");
         assertEquals(10, pay);
     }
+
+
 
 }
